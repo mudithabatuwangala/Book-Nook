84cfb1221750fe254dd9e74fbf9d0e286410765a
MudithaTharuka <mudithawyb234432@gmail.com>
2024-07-05 02:04:16 +0530

    Added the logo on the splash

--- Book-Nook/Splash.cs
+++ Book-Nook/Splash.cs
@@ -16,5 +16,15 @@ namespace Book_Nook
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
