f1c00c01e8d0b1b9489ea2df76c5f527c932ce8a
MudithaTharuka <mudithawyb234432@gmail.com>
2024-07-05 12:31:31 +0530

    Added the text on the splash

--- Book-Nook/Splash.Designer.cs
+++ Book-Nook/Splash.Designer.cs
@@ -29,35 +29,83 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.splashImage = new System.Windows.Forms.PictureBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.copyright = new System.Windows.Forms.TextBox();
+            this.splashDescription = new System.Windows.Forms.TextBox();
+            this.applicationName = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splashImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox2
+            // splashImage
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(248, -2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(354, 403);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.splashImage.Image = ((System.Drawing.Image)(resources.GetObject("splashImage.Image")));
+            this.splashImage.Location = new System.Drawing.Point(248, -2);
+            this.splashImage.Name = "splashImage";
+            this.splashImage.Size = new System.Drawing.Size(354, 403);
+            this.splashImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.splashImage.TabIndex = 1;
+            this.splashImage.TabStop = false;
             // 
-            // pictureBox3
+            // logo
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(12, 12);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(60, 60);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo.TabIndex = 2;
+            this.logo.TabStop = false;
+            // 
+            // copyright
+            // 
+            this.copyright.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.copyright.Font = new System.Drawing.Font("Tahoma", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyright.ForeColor = System.Drawing.Color.Indigo;
+            this.copyright.Location = new System.Drawing.Point(12, 184);
+            this.copyright.Multiline = true;
+            this.copyright.Name = "copyright";
+            this.copyright.Size = new System.Drawing.Size(168, 29);
+            this.copyright.TabIndex = 7;
+            this.copyright.Text = "© 2024 Muditha Batuwangala\r\nAll right reserved";
+            // 
+            // splashDescription
+            // 
+            this.splashDescription.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.splashDescription.Font = new System.Drawing.Font("Tahoma", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splashDescription.ForeColor = System.Drawing.Color.Indigo;
+            this.splashDescription.Location = new System.Drawing.Point(12, 238);
+            this.splashDescription.Multiline = true;
+            this.splashDescription.Name = "splashDescription";
+            this.splashDescription.Size = new System.Drawing.Size(213, 125);
+            this.splashDescription.TabIndex = 6;
+            this.splashDescription.Text = resources.GetString("splashDescription.Text");
+            // 
+            // applicationName
+            // 
+            this.applicationName.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.applicationName.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.applicationName.ForeColor = System.Drawing.Color.Indigo;
+            this.applicationName.Location = new System.Drawing.Point(12, 93);
+            this.applicationName.Name = "applicationName";
+            this.applicationName.Size = new System.Drawing.Size(144, 36);
+            this.applicationName.TabIndex = 8;
+            this.applicationName.Text = "Book Nook";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Indigo;
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 21);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Uncork a Universe";
             // 
             // pictureBox1
             // 
@@ -70,47 +118,25 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.textBox2.Font = new System.Drawing.Font("Adobe Heiti Std R", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.textBox2.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.textBox2.Location = new System.Drawing.Point(12, 187);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(168, 29);
-            this.textBox2.TabIndex = 7;
-            this.textBox2.Text = "© 2024 Muditha Batuwangala\r\nAll right reserved";
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.textBox1.Location = new System.Drawing.Point(12, 271);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(168, 48);
-            this.textBox1.TabIndex = 6;
-            this.textBox1.Text = "Sample Test";
-            // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.ClientSize = new System.Drawing.Size(600, 400);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.applicationName);
+            this.Controls.Add(this.copyright);
+            this.Controls.Add(this.splashDescription);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.splashImage);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splashImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -119,11 +145,13 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox splashImage;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.TextBox copyright;
+        private System.Windows.Forms.TextBox splashDescription;
+        private System.Windows.Forms.TextBox applicationName;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
